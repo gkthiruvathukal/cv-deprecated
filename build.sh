dbccf6195aa190d8cfc357278f747379ce3eab68
gkthiruvathukal <gkthiruvathukal@localhost>
2012-01-09 09:51:09 -0600

    miscellaneous corrections

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 PROJECT=gkt-cv
 
-markdown2pdf --template=fullpage.tex \
-	--variable version=1.4 \
-	$(PROJECT).md -o $(PROJECT).pdf
+markdown2pdf --template=fullpage.tex --variable version=1.4 $PROJECT.md -o $PROJECT.pdf
