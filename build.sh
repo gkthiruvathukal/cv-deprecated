dc3012ee402086878bfa24675c39a9bb960be3a8
George K. Thiruvathukal <thiruvathukal@gmail.com>
2013-08-14 15:34:10 -0500

    deploy process

--- build.sh
+++ build.sh
@@ -4,13 +4,24 @@ cat $(cat cv.in) > build/cv.markdown
 # Note: This drops the heading level for consistency with my other
 # web pages.
 
-cat $(cat cv.in) | sed -e "s/^#/###/" > build/cv-web.markdown
+cat $(cat cv.in) > build/cv.markdown
+cat $(cat cv.in) | sed -e "s/^#/###/" > build/cv.jekyll.markdown
 
 pushd build
+
+# Regular TeX PDF  Build
 pandoc --template=../fullpage.tex --variable version=1.4 cv.markdown -o cv.pdf
+
+# Word build
 pandoc cv.markdown -o cv.docx
-pandoc cv-web.markdown -o cv-raw.html
-cat ../cv-yaml.in cv-raw.html > cv.html
+
+# Regular HTML build
+pandoc cv.markdown -o cv.html
+
+# Build for use with my web site.
+pandoc cv.jekyll.markdown -o cv.jekyll.html.in
+cat ../cv-yaml.in cv.jekyll.html.in > cv.jekyll.html
+rm cv.jekyll.html.in
 popd
 
 if [ -d ../thiruvathukal.com/cv ]; then
